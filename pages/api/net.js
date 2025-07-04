757eaaba3fcb62bae113e604e1f5e47a13f4a77f
CT <tranthinhkhtt123@gmail.com>
2025-07-04 16:27:44 +0700

    Thêm API và giao diện biểu đồ

---
+++ pages/api/net.js
@@ -0,0 +1,19 @@
+let latestData = { temp: 0, time: new Date().toISOString() };
+
+export default function handler(req, res) {
+  if (req.method === "POST") {
+    const { temp } = req.body;
+    latestData = {
+      temp,
+      time: new Date().toISOString()
+    };
+    console.log("ESP gửi:", latestData);
+    return res.status(200).json({ message: "Đã nhận", data: latestData });
+  }
+
+  if (req.method === "GET") {
+    return res.status(200).json(latestData);
+  }
+
+  return res.status(405).json({ message: "Method not allowed" });
+}
